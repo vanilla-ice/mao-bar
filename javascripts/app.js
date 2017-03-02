8bf39c4e9a2a6fc80bf02b3d664f7ab06c9c270b
chunkey711 <chunkey95@gmail.com>
2017-03-02 20:59:04 +0300

    basket

--- javascripts/app.js
+++ javascripts/app.js
@@ -182,7 +182,20 @@ jade_debug.shift();;return buf.join("");
 });
 
 require.register("initialize.js", function(exports, require, module) {
-"use strict";
+'use strict';
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _jquery2.default)(document).ready(function () {
+
+  (0, _jquery2.default)('.my-order').on('click', function () {
+    (0, _jquery2.default)('.popup').toggleClass('popup-opened');
+  });
+});
 
 });
 
